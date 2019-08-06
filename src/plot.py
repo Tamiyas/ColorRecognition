4b42a4811c490e87273e4e0b71680a4eb910897a
T.Miyaji <thomath0901@gmail.com>
2019-08-06 13:35:09 +0900

    graphviz ver.のplot関数を追加

--- src/plot.py
+++ src/plot.py
@@ -1,4 +1,6 @@
 from dtreeviz.trees import dtreeviz
+import graphviz
+from sklearn.tree import export_graphviz
 from train import read_train_data
 from train import optimize
 from train import predict
@@ -14,7 +16,16 @@ def visualize(clf):
     viz.view()
 
 
+def plot(clf):
+    (train, target) = read_train_data()
+    dot_data = export_graphviz(
+        clf, class_names=['yellow', 'green', 'red', 'blue', 'black', 'white'], feature_names=['Hue', 'Saturation', 'Value'], filled=True, rounded=True, out_file=None)
+    graph = graphviz.Source(dot_data)
+    graph.render('../docs/tree', format='png')
+
+
 if __name__ == "__main__":
     clf = optimize()
     print(predict(clf))
+    plot(clf)
     visualize(clf)
